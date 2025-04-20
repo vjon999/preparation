8eed9daea4d8fcd21b23e69800da26ae69d8d7c3
venkatesh <vemkateshjonna999@gmail.com>
2025-04-20 13:07:27 +0530

    Initial commit

---
+++ src/test/java/com/example/preparation/PreparationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.preparation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreparationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
